7553be753acfe471261fe458db2fc9b2947ead22
Gregory Moyerbrailean <gregory.moyerbrailean@gmail.com>
2016-03-22 14:21:11 -0700

    Added sidebar with text

--- ase-forest/ui.R
+++ ase-forest/ui.R
@@ -9,7 +9,20 @@ shinyUI(fluidPage(
     ),
   
   fluidRow(
-    column(2),
+    sidebarPanel(withTags(
+      h2("cASE Forest Plots")),
+      h5("Click on a table entry to generate a plot in the top right corner."),
+      helpText(HTML("<br />
+                    <p>Left plot: Forest plot of ASE &beta;s for selected cASE SNP. The 
+                    ASE &beta; is displayed for each treatment in which the SNP was tested
+                    for that cell type. Error bars indicate the 95% confidence interval from 
+                    <a href='https://github.com/piquelab/quasar'>QuASAR</a>.</p>
+                    <br />
+                    <p>Right plot: The selected cASE SNP is represented as two points
+                    (treatment and control) connected by a line, plotted based on the
+                    expression of each allele. The dotted lines represent constant
+                    expression levels.</p>")), 
+      width=4),
     column(4, plotOutput("forest")),
     column(4, plotOutput("iase")),
     column(2)
