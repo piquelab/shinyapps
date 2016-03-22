030701d665cd097575803185de89e822dfcccdbf
Gregory Moyerbrailean <gregory.moyerbrailean@gmail.com>
2016-03-22 13:55:12 -0700

    Added table rows and updated sidebar

--- iase-arrow/helpers.R
+++ iase-arrow/helpers.R
@@ -10,7 +10,8 @@ threshLines <- data.frame( x = seq(0, 1, 0.01),
 prepTable <- function(data) {
   
   ## Drop unwanted columns
-  keep_cols <- c("rsID", "gene", "cell.type", "Treatment", "Control",
+  keep_cols <- c("rsID", "gene.name", "cell.type", "Treatment", "Control", "snplfc",
+                 "beta.t", "se.t", "qval.cov.t",
                  "ref.adj.t", "ref.adj.c", "alt.adj.t", "alt.adj.c")
   table <- data[, keep_cols]
   
@@ -27,15 +28,23 @@ sketch = htmltools::withTags(table(
     ## Top row
     tr(
       lapply(c("rsID", "Gene", "Cell Type", "Treatment", "Control"), th, rowspan="2"),
+      th(HTML("logFC<sup>&dagger;</sup>"), rowspan="2"),
+      th(colspan = 3, "Treatment ASE", class="th-center"),
       th(colspan = 2, "Reference Allele", class="th-center"),
       th(colspan = 2, 'Alternate Allele', class="th-center")
     ),
     ## Bottom Row
     tr(
+      ## Treatment ASE
+      th(HTML("&beta;"), class="alt"), 
+      th("Std Err", class="alt"),
+      th(HTML("<em>q</em>-value"), class="alt"), 
+      ## Reference Allele
       th("Treatment"), 
       th("Control"),
-      th("Treatment"),
-      th("Control")
+      ## Alternate Allele
+      th("Treatment", class="alt"),
+      th("Control", class="alt")
     )
   )
 ))
--- iase-arrow/ui.R
+++ iase-arrow/ui.R
@@ -11,14 +11,11 @@ shinyUI(fluidPage(
   fluidRow(
     sidebarPanel(withTags(
       h2("iASE Allele Plots")),
-      helpText("Click on a table entry to generate a plot in the top right corner.
-               Each iASE SNP is represented as two points (representing treatment 
-               and control expression) connected by a line (representing the 
-               fold-change between conditions). SNPs are plotted based on the 
-               expression of each allele, with the higher expressed allele in the 
-               treatment on the y-axis, and the lower allele on the x-axis. Points 
-               are colored by treatment (e.g., controls in black and grey), and the 
-               dotted lines represent constant expression levels 0.1, 1, and 10. For 
+      h5("Click on a table entry to generate a plot in the top right corner."),
+      helpText("Each iASE SNP is represented as two points (treatment and control) 
+               connected by a line, plotted based on the expression of each allele. 
+               Points are colored by treatment (e.g., controls in black and grey), 
+               and the dotted lines represent constant expression levels. For 
                ease of display, expression of SNPs <0.01 have been set to 0.01."), 
       width=4),
     column(1),
@@ -34,5 +31,6 @@ shinyUI(fluidPage(
   #   ),
   fluidRow(
     column(12, DT::dataTableOutput("table"))
-  )
+  ),
+  helpText(HTML("<sup>&dagger;</sup>SNP-based logFC"))
 ))
